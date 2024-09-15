f3b6f48cdda0eeef7e170b828850cd1d3e7654e2
Rohit <rohitsi2104@gmail.com>
2024-09-16 00:47:42 +0530

    updated url

--- backend/urls.py
+++ backend/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('api/', include('content.urls')),  # Include URLs from the content app
 ]
 if settings.DEBUG:
+    # handler500 = server_error
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_ROOT, documents_root= settings.STATIC_ROOT)
